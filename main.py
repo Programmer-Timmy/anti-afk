a72c1342e0e0733a0bcc412b229f85f65d141f72
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-10-28 01:33:36 +0200

    text update

--- main.py
+++ main.py
@@ -1,46 +1,58 @@
 import tkinter as tk
 import keyboard
+from tkinter import ttk
 import time
+import threading
+
 
 afk = False
-key_simulation_enabled = False
+simulation_thread = None
 
 def toggle_afk():
-    global afk, key_simulation_enabled
+    global afk, simulation_thread
     if afk:
         afk = False
-        key_simulation_enabled = False  # Disable key simulation
+        if simulation_thread:
+            simulation_thread.stop()
+        update_ui()
     else:
         afk = True
-        key_simulation_enabled = True  # Enable key simulation
-    update_ui()
+        simulation_thread = SimulationThread()
+        simulation_thread.start()
+        update_ui()
+
+class SimulationThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self._stop_event = threading.Event()
+
+    def run(self):
+        while not self._stop_event.is_set():
+            hold_and_release_key('w', 0.1)
+            time.sleep(1)
+            hold_and_release_key('a', 0.1)
+            time.sleep(1)
+            hold_and_release_key('s', 0.1)
+            time.sleep(1)
+            hold_and_release_key('d', 0.1)
+            time.sleep(1)
+
+    def stop(self):
+        self._stop_event.set()
 
 def hold_and_release_key(key, duration):
     keyboard.press(key)
     time.sleep(duration)
     keyboard.release(key)
 
-def simulate_keypress():
-    if afk and key_simulation_enabled:
-        hold_and_release_key('w', 0.1)
-        time.sleep(1)
-        hold_and_release_key('a', 0.1)
-        time.sleep(1)
-        hold_and_release_key('s', 0.1)
-        time.sleep(1)
-        hold_and_release_key('d', 0.1)
-        time.sleep(1)
-        if key_simulation_enabled:  # Check if key simulation should continue
-            label.after(100, simulate_keypress)
-
 def update_ui():
     if afk:
-        label.config(text="You are AFK now.")
-        button.config(text="I'm back")
-        simulate_keypress()
+        status_var.set("You are AFK now.")
+        button.config(text="I'm back", command=toggle_afk)
     else:
-        label.config(text="Press the button to go AFK. Or press F6")
-        button.config(text="Go AFK")
+        status_var.set("Press the button to go AFK or press F6")
+        button.config(text="Go AFK", command=toggle_afk)
+
 def toggle_afk_with_f6(event):
     toggle_afk()
 
@@ -50,12 +62,19 @@ root.geometry("400x200+100+100")
 root.resizable(False, False)
 root.attributes("-topmost", True)
 
-label = tk.Label(root, text="Press the button to go AFK.a Or press F6")
-label.pack()
+# Create a StringVar to hold the status text
+status_var = tk.StringVar()
+status_var.set("Press the button to go AFK or press F6")
 
-button = tk.Button(root, text="Go AFK", command=toggle_afk)
+# Create and style the label using the StringVar
+label = tk.Label(root, textvariable=status_var, font=("Helvetica", 14), bg="lightgray")
+label.pack(pady=20)
+
+# Create and style the button
+button = ttk.Button(root, text="Go AFK", command=toggle_afk, style="TButton")
 button.pack()
 
+# Register the F6 key press event
 keyboard.on_press_key("F6", toggle_afk_with_f6)
 
 root.mainloop()
